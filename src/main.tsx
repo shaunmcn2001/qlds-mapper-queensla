30e2caa41812658f85fc3633322c5abee44683bb
shaunmcn2001 <219618116+shaunmcn2001@users.noreply.github.com>
2025-09-08 07:05:39 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { ErrorBoundary } from "react-error-boundary";
-import "@github/spark/spark"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ToastProvider } from "./components/Toast";
 
-import App from './App.tsx'
-import { ErrorFallback } from './ErrorFallback.tsx'
-
-import "./main.css"
-import "./styles/theme.css"
-import "./index.css"
-
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-)
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <ToastProvider>
+    <App />
+  </ToastProvider>
+);
